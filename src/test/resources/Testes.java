fadc8fe2996860bfa01f562fcd624bc7d25490e7
Alini Gottardi <alini.gottardi@gmail.com>
2023-11-20 18:11:34 -0300

    Atualizando caminho de arquivo

--- src/test/resources/Testes.java
+++ src/test/resources/Testes.java
@@ -1,18 +1,12 @@
-package dev.alinigottardi;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.io.IOException;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Testes {
 
     @Test
-    public void PrimeiroTeste() throws IOException, InterruptedException {
+    public void PrimeiroTeste() {
         WebDriver driver = new FirefoxDriver();
         String URL = "http://www.google.com";
         driver.get(URL);
